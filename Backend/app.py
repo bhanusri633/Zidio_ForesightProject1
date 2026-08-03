ad958c1d06c52540f52c26ecf343ab60830a2735
bhanusri633 <bhanusridasoju08@gmail.com>
2026-08-03 16:16:08 +0530

    Completed AI Sales Forecasting Dashboard

--- Backend/app.py
+++ Backend/app.py
@@ -1,19 +1,27 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import numpy as np
+import pandas as pd
 
 # Create Flask app
 app = Flask(__name__)
+CORS(app)
 
 # Load trained model
 model = joblib.load("sales_forecast_model.pkl")
 
-# Home page
+# Load cleaned dataset
+df = pd.read_csv("../Data/cleaned_retail_data.csv")
+
+# ---------------- Home ----------------
+
 @app.route("/")
 def home():
     return "Sales Forecast API is Running!"
 
-# Prediction API
+# ---------------- Prediction API ----------------
+
 @app.route("/predict", methods=["POST"])
 def predict():
 
@@ -33,6 +41,59 @@ def predict():
         "Predicted Sales": float(prediction[0])
     })
 
-# Run the server
+# ---------------- Dashboard API ----------------
+
+@app.route("/dashboard", methods=["GET"])
+def dashboard():
+
+    total_sales = round(df["TotalAmount"].sum(), 2)
+
+    total_orders = int(df["Invoice"].nunique())
+
+    total_countries = int(df["Country"].nunique())
+
+    average_order = round(total_sales / total_orders, 2)
+
+    monthly_sales = (
+        df.groupby("Month")["TotalAmount"]
+        .sum()
+        .reset_index()
+    )
+
+    country_sales = (
+        df.groupby("Country")["TotalAmount"]
+        .sum()
+        .sort_values(ascending=False)
+        .head(5)
+        .reset_index()
+    )
+
+    top_products = (
+        df.groupby("Description")["Quantity"]
+        .sum()
+        .sort_values(ascending=False)
+        .head(5)
+        .reset_index()
+    )
+
+    return jsonify({
+
+        "kpis": {
+            "sales": total_sales,
+            "orders": total_orders,
+            "countries": total_countries,
+            "average": average_order
+        },
+
+        "monthly": monthly_sales.to_dict(orient="records"),
+
+        "country": country_sales.to_dict(orient="records"),
+
+        "products": top_products.to_dict(orient="records")
+
+    })
+
+# ---------------- Run ----------------
+
 if __name__ == "__main__":
     app.run(debug=True)
