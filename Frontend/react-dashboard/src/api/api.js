449fb7101d11c09adf4051093e9c671d3ff0d970
Bhanusri Dasoju <bhanusridasoju08@gmail.com>
2026-08-11 17:39:34 +0530

    Change API base URL to production endpoint

--- Frontend/react-dashboard/src/api/api.js
+++ Frontend/react-dashboard/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000"
+  baseURL: "https://zidio-foresightproject1.onrender.com"
 });
 
 export const getDashboardData = () => API.get("/dashboard");
