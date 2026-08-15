7031c01b7148a127bef1643fd65ca2c7a08dc2cc
Bhanusri Dasoju <bhanusridasoju08@gmail.com>
2026-08-15 10:39:30 +0530

    Update api.js

--- Frontend/react-dashboard/src/api/api.js
+++ Frontend/react-dashboard/src/api/api.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://zidio-foresightproject1.onrender.com"
+  baseURL: "https://zidio-foresightproject1.onrender.com",
+  timeout: 30000,
 });
 
 export const getDashboardData = () => API.get("/dashboard");
 
 export const predictSales = (data) =>
   API.post("/predict", data);
+
+export default API;
